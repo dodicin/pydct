ea608f96873e63a66f37795cdfd1318f0364ae23
dodicin <nostalgia.driver@gmail.com>
2019-05-31 20:36:27 +0200

    finished the compare script

--- compare.py
+++ compare.py
@@ -1,12 +1,42 @@
+import time
+import os
+import csv
+
 import pydct as p
 import numpy as np
 from scipy.fftpack import dctn
 
-def compare():
+def write_data():
+    data_file = "data/data.csv"
+    if not os.path.exists("data/"):
+        os.makedirs("data/")
+
+    if not os.path.exists(data_file):
+        with open(data_file, "a") as logfile:
+            header_writer = csv.writer(logfile, delimiter=",")
+            header_writer.writerow(["size", "library", "time"])
+
+    with open(data_file, "a") as logfile:
+        for row in results:
+            logfile.write(", ".join(row) + "\n")
+
+
+results = []
+for i in range(1, 13):
+    mat = np.random.rand(2**i, 2**i)
     
-    for i in range(1, 11):
-        mat = np.random.rand(2**i, 2**i)
-        print(mat)
+    for j in range(10):
 
+        t0 = time.process_time()
+        _ = dctn(mat, type = 2, norm = 'ortho')
+        t = time.process_time() - t0
+        
+        results.append(["{}x{}".format(2**i, 2**i), "scipy", str(t)])
 
-compare()
+        t0 = time.process_time()
+        _ = p.dct2(mat)
+        t = time.process_time() - t0
+        
+        results.append(["{}x{}".format(2**i, 2**i), "pydct", str(t)])
+
+write_data()
